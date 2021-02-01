8c993e1db169705c240e47b042e6278cf7207a18
Bizhan Nauryzbay <nauryzbaybizhan@gmail.com>
2021-02-01 23:45:21 +0600

    add percentile

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,11 +1,12 @@
 package test;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import test.model.Ticket;
 import test.util.TicketUtil;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -15,23 +16,24 @@ public class Main {
 
     public static void main(String[] args) {
         List<Ticket> ticketList = new ArrayList<>();
-        String fileName = "tickets.json";
+        var fileName = "tickets.json";
         try {
-            JsonParser jsonParser = new JsonParser();
+            var jsonParser = new JsonParser();
             String someJson = Files.readString(Paths.get(fileName));
-            Gson gson = new Gson();
-            JsonObject ticketObj = jsonParser.parse(someJson).getAsJsonObject();
-            JsonArray tickets = ticketObj.get("tickets").getAsJsonArray();
+            var gson = new Gson();
+            var ticketObj = jsonParser.parse(someJson).getAsJsonObject();
+            var tickets = ticketObj.get("tickets").getAsJsonArray();
             for (JsonElement jsonElement : tickets) {
-                Ticket ticket = gson.fromJson(jsonElement, Ticket.class);
+                var ticket = gson.fromJson(jsonElement, Ticket.class);
                 ticketList.add(ticket);
             }
             String s = TicketUtil.averageFlight(ticketList);
-            String ave = "Среднее время полета между городами Владивосток и Тель-Авив займет " + s + " минут(ы)";
-            System.out.println(ave);
-            try (PrintWriter printWriter = new PrintWriter("result.txt")) {
-                printWriter.write(ave);
-            }
+            var aveStr = "Среднее время полета между городами Владивосток и Тель-Авив займет " + s + " минут(ы)";
+            var percentile = 90;
+            var percStr = percentile + "-й процентиль времени полета между городами  Владивосток и Тель-Авив равен "
+                    + TicketUtil.percentileFlight(ticketList, percentile);
+            System.out.println(aveStr);
+            System.out.println(percStr);
         } catch (IOException e) {
             System.out.println("Не найден " + fileName);
             e.printStackTrace();
--- src/main/java/test/util/TicketUtil.java
+++ src/main/java/test/util/TicketUtil.java
@@ -2,23 +2,29 @@ package test.util;
 
 import test.model.Ticket;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class TicketUtil {
 
     public static String averageFlight(List<Ticket> tickets) {
-        long count = tickets.stream().mapToLong(ticket -> getMinutesOfFlight(getDateFromTicketStr(ticket.departureDate, ticket.departureTime),
+        var count = tickets.stream().mapToLong(ticket -> getMinutesOfFlight(getDateFromTicketStr(ticket.departureDate, ticket.departureTime),
                 getDateFromTicketStr(ticket.arrivalDate, ticket.arrivalTime))).sum();
         return String.valueOf(count / tickets.size());
     }
 
-    public static Date getDateFromTicketStr(String date, String time) {
+    public static long percentileFlight(List<Ticket> tickets, double percentile) {
+        List<Long> collect = tickets.stream().map(ticket -> getMinutesOfFlight(getDateFromTicketStr(ticket.departureDate, ticket.departureTime),
+                getDateFromTicketStr(ticket.arrivalDate, ticket.arrivalTime))).collect(Collectors.toList());
+        Collections.sort(collect);
+        var index = (int) Math.ceil(percentile / 100.0 * collect.size());
+        return collect.get(index-1);
+    }
+
+    private static Date getDateFromTicketStr(String date, String time) {
         try {
-            Calendar calendar = new GregorianCalendar();
+            var calendar = new GregorianCalendar();
             String[] splitDate = date.split("\\.");
             String[] splitTime = time.split(":");
             calendar.set(Integer.parseInt("20" + splitDate[2]), Integer.parseInt(splitDate[1]), Integer.parseInt(splitDate[0]),
@@ -29,7 +35,7 @@ public class TicketUtil {
         }
     }
 
-    public static long getMinutesOfFlight(Date departure, Date arrival) {
+    private static long getMinutesOfFlight(Date departure, Date arrival) {
         long diff = arrival.getTime() - departure.getTime();
         return TimeUnit.MILLISECONDS.toMinutes(diff);
     }
